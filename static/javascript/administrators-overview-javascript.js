bac59c4ba66e8719bfe4d1b127edcfaad2a39970
Eline-1007803 <1007803@hr.nl>
2025-02-04 22:44:15 +0100

    added pop ups for each button

--- static/javascript/administrators-overview-javascript.js
+++ static/javascript/administrators-overview-javascript.js
@@ -1,3 +1,7 @@
+
+// Function for pop up to show up when you click the button
+
+// Details
 details = document.querySelector(".details-button")
 
 function detailsPopUp () {
@@ -7,3 +11,22 @@ function detailsPopUp () {
 }
 
 document.querySelector(".details-button").addEventListener("click", detailsPopUp);
+
+// Edit
+function editPopUp () {
+        console.log('yaas');
+        document.querySelector(".js-edit").classList.remove("hide");
+
+}
+
+document.querySelector(".edit-button").addEventListener("click", editPopUp);
+
+
+// Delete
+function deletePopUp () {
+        console.log('yaas');
+        document.querySelector(".js-delete").classList.remove("hide");
+
+}
+
+document.querySelector(".delete-button").addEventListener("click", deletePopUp);
