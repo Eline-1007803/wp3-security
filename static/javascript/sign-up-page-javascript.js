6d784a39109ca95435aec531f571e135ff13c981
Eline-1007803 <1007803@hr.nl>
2025-02-05 23:47:24 +0100

    fixed annoying bug. form doesn't change anymore when supervisor questions open

--- static/javascript/sign-up-page-javascript.js
+++ static/javascript/sign-up-page-javascript.js
@@ -5,10 +5,13 @@ function showSupervisorForm () {
     if (checkbox.checked === true) {
         console.log("you go girlie")
         document.querySelector(".js-supervisor-info").classList.remove("hide");
+        document.querySelector(".js-space").classList.remove("hide");
     }
     else {
         document.querySelector(".js-supervisor-info").classList.add("hide");
+        document.querySelector(".js-space").classList.add("hide");
     }
 }
 
 checkbox.addEventListener("change", showSupervisorForm);
+
