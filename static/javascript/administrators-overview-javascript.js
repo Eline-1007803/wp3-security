8db624f5cb3dcc120fc10418972466072385af9f
Eline-1007803 <1007803@hr.nl>
2025-02-06 23:40:26 +0100

    instead of a element made javascript function with for loop

--- static/javascript/administrators-overview-javascript.js
+++ static/javascript/administrators-overview-javascript.js
@@ -4,15 +4,14 @@
 // Add
 document.querySelector(".add-administrator-button").addEventListener("click", ()=> {
         console.log('yaas');
-        document.querySelector(".background").classList.remove("hide");
+        document.querySelector(".js-background").classList.remove("hide");
         document.querySelector(".js-add").classList.remove("hide");
-
 });
 
 // Details
 function detailsPopUp () {
         console.log('yaas');
-        document.querySelector(".background").classList.remove("hide");
+        document.querySelector(".js-background").classList.remove("hide");
         document.querySelector(".js-details").classList.remove("hide");
 }
 
@@ -21,9 +20,8 @@ document.querySelector(".details-button").addEventListener("click", detailsPopUp
 // Edit
 function editPopUp () {
         console.log('yaas');
-        document.querySelector(".background").classList.remove("hide");
+        document.querySelector(".js-background").classList.remove("hide");
         document.querySelector(".js-edit").classList.remove("hide");
-
 }
 
 document.querySelector(".edit-button").addEventListener("click", editPopUp);
@@ -32,9 +30,21 @@ document.querySelector(".edit-button").addEventListener("click", editPopUp);
 // Delete
 function deletePopUp () {
         console.log('yaas');
-        document.querySelector(".background").classList.remove("hide");
+        document.querySelector(".js-background").classList.remove("hide");
         document.querySelector(".js-delete").classList.remove("hide");
-
 }
 
 document.querySelector(".delete-button").addEventListener("click", deletePopUp);
+
+function closePopUp () {
+        document.querySelector(".js-background").classList.add("hide");
+        document.querySelector(".js-add").classList.add("hide");
+        document.querySelector(".js-details").classList.add("hide");
+        document.querySelector(".js-edit").classList.add("hide");
+        document.querySelector(".js-delete").classList.add("hide");
+}
+
+document.querySelectorAll(".js-cross-image")
+    .forEach(element => {
+            element.addEventListener("click", closePopUp);
+    });
