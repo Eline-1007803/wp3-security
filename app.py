088535db5e3687f35fe9e626558df0ef309f7586
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-02-10 13:40:27 +0100

    update aan app.py: route gemaakt voor onderzoek aanvragen voor organisatie

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def homepage():
     pass
 
+@app.route("/onderzoekaanvragen")
+def onderzoek_aanvragen_organisatie():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
