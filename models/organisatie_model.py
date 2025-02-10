a2aced4fe1457a82b4872dca892891c744d740c5
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-02-10 12:05:04 +0100

    parameters toegevoegd aan functie om onderzoek aan te vragen

--- models/organisatie_model.py
+++ models/organisatie_model.py
@@ -5,5 +5,5 @@ class Organisatie:
         database = Database("./databases/database.db")
         self.cursor, self.con = database.connect_db()
     
-    def insert_onderzoek():
+    def insert_onderzoek(titel,beschjrijving,datum,typeonderzoek,vergoeding,hoeveel_vergoeding,beperking,leeftijd):
         pass
