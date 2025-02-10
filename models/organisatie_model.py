e21e68d082c637ad3de2717373167889a7381cb4
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-02-10 23:41:37 +0100

    update aan functie: query toegevoegd binnen functie insert_onderzoek

--- models/organisatie_model.py
+++ models/organisatie_model.py
@@ -5,5 +5,9 @@ class Organisatie:
         database = Database("./databases/database.db")
         self.cursor, self.con = database.connect_db()
     
-    def insert_onderzoek(titel,beschjrijving,datum,typeonderzoek,vergoeding,hoeveel_vergoeding,beperking,leeftijd):
-        pass
+    def insert_onderzoek(self,titel,beschjrijving,datum_vanaf,datum_tot,typeonderzoek,vergoeding,hoeveel_vergoeding,leeftijd_vanaf,leeftijd_tot,beperking_id):
+        self.cursor.execute(
+            "INSERT into onderzoeken (titel,beschrijving,datum_vanaf,datum_tot,type,locatie,met_beloning,beloning,beperking,leeftijd_van,leeftijd_tot,beperking_id) VALUES (?,?,?,?,?,?,?,?,?,?)",
+            (titel,beschjrijving,datum_vanaf,datum_tot,typeonderzoek,vergoeding,hoeveel_vergoeding,leeftijd_vanaf,leeftijd_tot,beperking_id))
+        self.con.commit()
+        return True
