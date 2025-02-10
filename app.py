a05d9c45ee2d48f99a743cbbd6cbe5cbc2f42f09
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-02-10 23:33:34 +0100

    update aan onderzoek aanvragen route:  code geschreven om values te krijgen van input velden

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import *
 app = Flask(__name__)
+from models.organisatie_model import Organisatie
 
 @app.route('/')
 def homepage():
     pass
 
-@app.route("/onderzoekaanvragen")
+@app.route("/onderzoekaanvragen",methods="POST")
 def onderzoek_aanvragen_organisatie():
-    pass
+    if request.method == "POST":
+        title = request.form.get("titel")
+        beschrijving = request.form.get("beschrijving")
+        datum_vanaf = request.form.get("datumvanaf")
+        datum_tot = request.form.get("datumtot")
+        type_onderzoek = request.form.get("typeonderzoek")
+        met_beloning = request.form.get("metbeloning")
+        hoeveel_beloning = request.form.get("beloning")
+        type_disability = request.form.get("disability-type-input")
+        leeftijd_van = request.form.get("leeftijdvan")
+        leeftijd_tot = request.form.get("leeftijdtot")
+        if met_beloning == "on":
+            met_beloning = 1
+        onderzoek = organisatie.tobeadded(title,beschrijving,datum_vanaf,datum_tot,type_onderzoek,met_beloning,hoeveel_beloning,type_disability,leeftijd_van,leeftijd_tot)
+        return redirect(url_for("onderzoek_aanvragen_organisatie"))
+    else:
+        return render_template("onderzoek_aanvraag__organisatie.html")
 
 if __name__ == '__main__':
+    organisatie = Organisatie()
     app.run(debug=True)
