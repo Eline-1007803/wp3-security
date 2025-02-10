8d47ab9a05aa5f61227d6bfb205b3125bd47d1f7
Djanou-1099994 <djanouoverweg@gmail.com>
2025-02-10 22:10:20 +0100

    feature: made ervaringsdeskundigen_model.py with the option to get all pending ervaringsdeskundigen

---
+++ models/ervaringsdeskundigen_model.py
@@ -0,0 +1,16 @@
+from models.database_conection import Database
+
+class Ervaringsdeskundigen:
+    def __init__(self):
+        database = Database("./databases/database.db")
+        self.cursor, self.con = database.connect_db()
+
+    def get_all_pending(self):
+        result = self.cursor.execute(
+            """ SELECT ervaringsdeskundigen.*, alle_beperkingen.naam, 
+                (strftime('%Y', 'now') - strftime('%Y', ervaringsdeskundigen.geboortedatum) - (strftime('%m-%d', 'now') < strftime('%m-%d', ervaringsdeskundigen.geboortedatum))) AS leeftijd
+                FROM ervaringsdeskundigen 
+                full join geregistreerde_beperkingen on (ervaringsdeskundigen.ervaringsdeskundige_id=geregistreerde_beperkingen.ervaringsdeskundige_id)
+                full join alle_beperkingen on (geregistreerde_beperkingen.beperking_id = alle_beperkingen.beperking_id)
+                WHERE status = 'nieuw'""").fetchall()
+        return result
