ee45541d2688153060773a1f8fd2f624a3afc6c6
Djanou-1099994 <djanouoverweg@gmail.com>
2025-02-11 15:39:13 +0100

    feature: added js to the dashboard so it gets updated every 3 seconds

--- static/javascript/beheerders_dashbord.js
+++ static/javascript/beheerders_dashbord.js
@@ -1,27 +1,65 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
-var btn3 = document.getElementById("myBtn3");
-var span = document.getElementsByClassName("close")[0];
+function refresh_deskundigen(data) {
+  let deskundigen_collection = document.getElementById('deskundigen');
+  deskundigen_collection.innerHTML = '';
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // Ik doe de aanname dat de data een lijst is van onderzoeksaanvragen
+  let deskundigen = data.deskundigen;
+  let deskundigen_html = '';
+  for (let i = 0; i < deskundigen.length; i++) {
+      let deskundigenElement = deskundigen[i];
 
-btn2.onclick = function() {
-  modal.style.display = "block";
-}
-
-btn3.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+      // Javascript kent Python-achtige "f" strings, waarin je variabelen kan vervangen
+      // Die lappen HTML plak ik achter elkaar in de nu lege deskundigen_html
+      deskundigen_html += `
+                <tr>
+                    <td>${deskundigenElement.voornaam}</td>
+                    <td>${deskundigenElement.leeftijd}</td>
+                    <td>${deskundigenElement.naam}</td>
+                </tr>
+      `;
+      // En uiteindelijk plak ik de hele HTML string in het element met id 'deskundigen'
+      deskundigen_collection.innerHTML = deskundigen_html;
   }
 }
+
+
+function get_deskundigen() {
+    fetch('/api/deskundigen')
+        .then(response => response.json())
+        .then(refresh_deskundigen);
+}
+
+get_deskundigen();
+
+setInterval(get_deskundigen, 3000)
+
+
+
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var btn2 = document.getElementById("myBtn2");
+// var btn3 = document.getElementById("myBtn3");
+// var span = document.getElementsByClassName("close")[0];
+//
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+//
+// btn2.onclick = function() {
+//   modal.style.display = "block";
+// }
+//
+// btn3.onclick = function() {
+//   modal.style.display = "block";
+// }
+//
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+//
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
