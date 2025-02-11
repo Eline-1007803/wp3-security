f9ff0623a15997821a3c8697cbe480edfd2e188d
Djanou-1099994 <djanouoverweg@gmail.com>
2025-02-11 15:36:12 +0100

    feature: made an api with GET for deskundigen

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import *
+from models import ervaringsdeskundigen_model
+
 app = Flask(__name__)
 app.secret_key = "wp3"
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('beheerders_dashboard.html')
+
+@app.route('/api/deskundigen')
+def get_deskundigen():
+    edm = ervaringsdeskundigen_model.Ervaringsdeskundigen()
+    result = edm.get_all_pending()
+    dictresult = []
+    for row in result:
+        dictresult.append(dict(row))
+    return {"deskundigen": dictresult}
 
 
 if __name__ == '__main__':
