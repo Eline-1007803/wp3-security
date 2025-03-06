eea69e17374470dff0ec0375c767fc648a410021
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-06 09:37:43 +0100

    pep8 verbetering aan app.py

--- app.py
+++ app.py
@@ -6,15 +6,22 @@ from lib.model.sign_up import SignUp
 
 
 app = Flask(__name__)
-from models import ervaringsdeskundigen_model, inschrijvingen_model, onderzoeken_model,organisatie_model
+from models import (
+    ervaringsdeskundigen_model,
+    inschrijvingen_model,
+    onderzoeken_model,
+    organisatie_model,
+)
 
 app = Flask(__name__)
 app.secret_key = "wp3"
 
+
 @app.route("/", methods=["GET"])
 def test():
     if request.method == "GET":
-        return jsonify({"response":"hallo"})
+        return jsonify({"response": "hallo"})
+
 
 @app.route("/api/administrators", methods=["GET"])
 def get_all_administrators():
@@ -22,12 +29,14 @@ def get_all_administrators():
     administrators = administrator_model.get_all_administrators()
     return jsonify(administrators)
 
+
 @app.route("/api/administrator/<administrator_id>", methods=["GET"])
 def get_administrator_by_id(administrator_id):
     administrator_model = Administrator()
     administrator = administrator_model.get_administrator_by_id(administrator_id)
     print(administrator)
     return jsonify(administrator)
+
 
 @app.route("/api/new-administrator", methods=["POST"])
 def add_administrator():
@@ -39,15 +48,19 @@ def add_administrator():
     new_administrator = administrator_model.add_administrator(fname, lname, email)
     return new_administrator, 201
 
+
 @app.route("/api/administrator/<administrator_id>", methods=["PATCH"])
 def update_administrator(administrator_id):
     voornaam = request.json["voornaam"]
     achternaam = request.json["achternaam"]
     email = request.json["email"]
     administrator_model = Administrator()
-    updated_administrator = administrator_model.update_administrator(voornaam, achternaam, email, administrator_id)
+    updated_administrator = administrator_model.update_administrator(
+        voornaam, achternaam, email, administrator_id
+    )
     print(updated_administrator)
     return updated_administrator
+
 
 @app.route("/api/administrator/<administrator_id>", methods=["DELETE"])
 def delete_administrator(administrator_id):
@@ -56,15 +69,18 @@ def delete_administrator(administrator_id):
     print(deleted_administrator)
     return jsonify("hallo")
 
+
 @app.route("/administrator-overview", methods=["GET"])
 def administrator_page():
     administrator_model = Administrator()
     administrators = administrator_model.get_all_administrators()
     return render_template("administrators-overview.html")
 
+
 @app.route("/expert-sign-up")
 def expert_sign_up():
     return render_template("sign-up-page.html")
+
 
 @app.route("/api/save-signup", methods=["POST"])
 def save_sign_up():
@@ -74,11 +90,13 @@ def save_sign_up():
     save_sign_up = signup_model.save_signup(fname, lname)
     return save_sign_up
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('beheerders_dashboard.html')
 
-@app.route('/api/deskundigen', methods=['GET'])
+@app.route("/dashboard")
+def dashboard():
+    return render_template("beheerders_dashboard.html")
+
+
+@app.route("/api/deskundigen", methods=["GET"])
 def get_deskundigen():
     edm = ervaringsdeskundigen_model.Ervaringsdeskundigen()
     result = edm.get_all_pending()
@@ -87,7 +105,8 @@ def get_deskundigen():
         dictresult.append(dict(row))
     return {"deskundigen": dictresult}
 
-@app.route('/api/inschrijvingen', methods=['GET'])
+
+@app.route("/api/inschrijvingen", methods=["GET"])
 def get_inschrijvingen():
     ism = inschrijvingen_model.Inschrijvingen()
     result = ism.get_all_pending()
@@ -96,7 +115,8 @@ def get_inschrijvingen():
         dictresult.append(dict(row))
     return {"inschrijvingen": dictresult}
 
-@app.route('/api/onderzoeken', methods=['GET'])
+
+@app.route("/api/onderzoeken", methods=["GET"])
 def get_onderzoeken():
     ozm = onderzoeken_model.Onderzoeken()
     result = ozm.get_all_pending()
@@ -105,93 +125,120 @@ def get_onderzoeken():
         dictresult.append(dict(row))
     return {"onderzoeken": dictresult}
 
-@app.route("/api/overzicht_onderzoeken",methods=["GET"])
-def overzicht_onderzoeken():
-    organisatie_id = 1 # for now
-    onderzoeken = organisatie.get_all_onderzoeken(organisatie_id)
-    return render_template("overzicht_onderzoeken.html",onderzoeken=onderzoeken)
 
-@app.route("/api/overzicht_onderzoeken/<onderzoek_id>",methods=["PATCH"])
+@app.route("/api/overzicht_onderzoeken", methods=["GET"])
+def overzicht_onderzoeken():
+    organisatie_id = 1  # for now
+    onderzoeken = organisatie.get_all_onderzoeken(organisatie_id)
+    return render_template("overzicht_onderzoeken.html", onderzoeken=onderzoeken)
+
+
+@app.route("/api/overzicht_onderzoeken/<onderzoek_id>", methods=["PATCH"])
 def update_onderzoek_gegevens(onderzoek_id):
     title = request.json["titel"]
     if title == "":
-        return jsonify("Titel can't be empty!"),400
-    
+        return jsonify("Title can't be empty!"), 400
+
     beschrijving = request.json["beschrijving"]
     if beschrijving == "":
-        return jsonify("Beschhijving can't be empty!"),400
-    
+        return jsonify("Beschrijving can't be empty!"), 400
+
     datum_vanaf = request.json["datumvanaf"]
-    date_vanaf = datetime.strptime(datum_vanaf,"%Y-%m-%d")
+    date_vanaf = datetime.strptime(datum_vanaf, "%Y-%m-%d")
     if datum_vanaf == "" or date_vanaf < datetime.now():
-        return jsonify("You have not chosen a date from or chosen a past date."),400
-    
+        return jsonify("You have not chosen a date from or chosen a past date."), 400
+
     datum_tot = request.json["datumtot"]
-    date_tot = datetime.strptime(datum_tot,"%Y-%m-%d")
+    date_tot = datetime.strptime(datum_tot, "%Y-%m-%d")
     if datum_tot == "" or date_tot < date_vanaf:
-        return jsonify("You have not chosen a date till or chosen a date before date from"),400
-    
-    organisatie_id = 1 #for now
-    updated_onderzoek_gegevens = organisatie.update_onderzoek(title,beschrijving,datum_vanaf,datum_tot,onderzoek_id,organisatie_id)
-    return jsonify(updated_onderzoek_gegevens),200
+        return (
+            jsonify(
+                "You have not chosen a date till or chosen a date before date from"
+            ),
+            400,
+        )
+
+    organisatie_id = 1  # for now
+    updated_onderzoek_gegevens = organisatie.update_onderzoek(
+        title, beschrijving, datum_vanaf, datum_tot, onderzoek_id, organisatie_id
+    )
+    return jsonify(updated_onderzoek_gegevens), 200
 
 
-@app.route("/onderzoekaanvragen",methods=["GET"])
+@app.route("/onderzoekaanvragen", methods=["GET"])
 def onderzoek_pagina():
     return render_template("onderzoek_aanvraag__organisatie.html")
 
-@app.route("/api/onderzoekaanvragen",methods=["POST"])
+
+@app.route("/api/onderzoekaanvragen", methods=["POST"])
 def onderzoek_aanvragen_organisatie():
     title = request.json["titel"]
     if title == "":
-        return jsonify("Titel can't be empty!"),400
+        return jsonify("Titel can't be empty!"), 400
 
     beschrijving = request.json["beschrijving"]
     if beschrijving == "":
-        return jsonify("Beschhijving can't be empty!"),400
+        return jsonify("Beschrijving can't be empty!"), 400
 
     datum_vanaf = request.json["datumvanaf"]
-    date_vanaf = datetime.strptime(datum_vanaf,"%Y-%m-%d")
+    date_vanaf = datetime.strptime(datum_vanaf, "%Y-%m-%d")
     if datum_vanaf == "" or date_vanaf < datetime.now():
-        return jsonify("You have not chosen a date from or chosen a past date."),400
-    
+        return jsonify("You have not chosen a date from or chosen a past date."), 400
+
     datum_tot = request.json["datumtot"]
-    date_tot = datetime.strptime(datum_tot,"%Y-%m-%d")
+    date_tot = datetime.strptime(datum_tot, "%Y-%m-%d")
     if datum_tot == "" or date_tot < date_vanaf:
-        return jsonify("You have not chosen a date till or chosen a date before date from"),400
-    
+        return (
+            jsonify(
+                "You have not chosen a date till or chosen a date before date from"
+            ),
+            400,
+        )
+
     if datum_vanaf == "":
-        return jsonify("Datum vanaf cant be empty!"),400
+        return jsonify("Datum vanaf cant be empty!"), 400
 
     datum_tot = request.json["datumtot"]
     if datum_tot == "":
-        return jsonify("Datum tot cant be empty!"),400
+        return jsonify("Datum tot cant be empty!"), 400
 
     type_onderzoek = request.json["typeonderzoek"]
     if type_onderzoek == "":
-        return jsonify("Kies type onderzoek!"),400
+        return jsonify("Kies type onderzoek!"), 400
 
     locatie = request.json["locatie_text"]
     met_beloning = request.json["metbeloning"]
     hoeveel_beloning = request.json["beloning"]
     type_disability = request.json["disability-type-input"]
     if type_disability == "":
-        return jsonify("Kies beperking!"),400
+        return jsonify("Kies beperking!"), 400
 
     leeftijd_van = request.json["leeftijdvan"]
     if leeftijd_van == "":
-        return jsonify("Je hebt geen leeftijd ingevoerd"),400
-    
+        return jsonify("Je hebt geen leeftijd ingevoerd"), 400
+
     leeftijd_tot = request.json["leeftijdtot"]
     if leeftijd_tot == "":
-        return jsonify("Kies leeftijd tot!"),400
+        return jsonify("Kies leeftijd tot!"), 400
 
     if met_beloning == "on":
         met_beloning = 1
     else:
         met_beloning = 0
-    organisatie_id = 1 #for now
-    onderzoek = organisatie.insert_onderzoek(title,beschrijving,datum_vanaf,datum_tot,type_onderzoek,locatie,met_beloning,hoeveel_beloning,leeftijd_van,leeftijd_tot,organisatie_id)
+    organisatie_id = 1  # for now
+    onderzoek = organisatie.insert_onderzoek(
+        title,
+        beschrijving,
+        datum_vanaf,
+        datum_tot,
+        type_onderzoek,
+        locatie,
+        met_beloning,
+        hoeveel_beloning,
+        leeftijd_van,
+        leeftijd_tot,
+        organisatie_id,
+    )
     onderzoek_id_opvragen = organisatie.get_last_onderzoek_id()
     onderzoek_id = int(onderzoek_id_opvragen[0])
     for disability in type_disability:
@@ -199,7 +246,6 @@ def onderzoek_aanvragen_organisatie():
     return jsonify(onderzoek), 201
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     organisatie = organisatie_model.Organisatie()
     app.run(debug=True)
