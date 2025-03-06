87ba05ce1ed303e1c3269d523e896407df951c37
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-06 09:19:37 +0100

    bugfix bij sql query en status toegevoeggd en  tekst verbetering bij beschikbaarheid

--- models/organisatie_model.py
+++ models/organisatie_model.py
@@ -63,7 +63,7 @@ class Organisatie:
         self, title, beschrijving, datumvanaf, datumtot, onderzoek_id, organisatie_id
     ):
         self.cursor.execute(
-            " UPDATE onderzoeken SET title = ?, beschrijving = ?, datumvanaf = ?,datumtot = ? WHERE organisatie_id = ?",
+            " UPDATE onderzoeken SET titel = ?, beschrijving = ?, datum_vanaf = ?,datum_tot = ? onderzoek_id = ? WHERE organisatie_id = ?",
             (title, beschrijving, datumvanaf, datumtot, onderzoek_id, organisatie_id),
         )
         self.con.commit()
