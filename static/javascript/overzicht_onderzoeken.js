0b94510d7316edbcbae0ef785e86be4320cf716b
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-07 18:07:27 +0100

    modal toegevoegd om onderzoek gegevens te kunnen wijzigen

---
+++ static/javascript/overzicht_onderzoeken.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("update_modal");
+
+var btn = document.getElementById("update_button");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
