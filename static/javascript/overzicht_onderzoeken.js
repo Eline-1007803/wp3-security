92ca6eed34a4f230011851374defebc8aebfce37
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-07 18:22:59 +0100

    modal gemaakt voor ingeschreven mensen

--- static/javascript/overzicht_onderzoeken.js
+++ static/javascript/overzicht_onderzoeken.js
@@ -1,19 +1,40 @@
-var modal = document.getElementById("update_modal");
+var update_modal = document.getElementById("update_modal");
 
-var btn = document.getElementById("update_button");
+var update_btn = document.getElementById("update_button");
 
-var span = document.getElementsByClassName("close")[0];
+var span_for_update_modal = document.getElementsByClassName("close_updatemodal")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
+update_btn.onclick = function() {
+  update_modal.style.display = "block";
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+span_for_update_modal.onclick = function() {
+  update_modal.style.display = "none";
 }
 
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == update_modal) {
+    update_modal.style.display = "none";
+  }
+}
+
+
+var user_modal = document.getElementById("users_modal");
+
+var users_btn = document.getElementById("users_button");
+
+var span_for_user_modal = document.getElementsByClassName("close_usersmodal")[0];
+
+users_btn.onclick = function() {
+  user_modal.style.display = "block";
+}
+
+span_for_user_modal.onclick = function() {
+  user_modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == user_modal) {
+    user_modal.style.display = "none";
   }
 }
