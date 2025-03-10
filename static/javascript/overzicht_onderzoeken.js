71c304f16c7ee2658a87bcbd11cf0397c5e486a2
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-10 13:38:15 +0100

    onnodige row in tabel weg gehaald

--- static/javascript/overzicht_onderzoeken.js
+++ static/javascript/overzicht_onderzoeken.js
@@ -1,45 +1,3 @@
-var update_modal = document.getElementById("update_modal");
-
-var update_btn = document.getElementById("update_button");
-
-var span_for_update_modal = document.getElementsByClassName("close_updatemodal")[0];
-
-update_btn.onclick = function() {
-  update_modal.style.display = "block";
-}
-
-span_for_update_modal.onclick = function() {
-  update_modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == update_modal) {
-    update_modal.style.display = "none";
-  }
-}
-
-
-var user_modal = document.getElementById("users_modal");
-
-var users_btn = document.getElementById("users_button");
-
-var span_for_user_modal = document.getElementsByClassName("close_usersmodal")[0];
-
-users_btn.onclick = function() {
-  user_modal.style.display = "block";
-}
-
-span_for_user_modal.onclick = function() {
-  user_modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == user_modal) {
-    user_modal.style.display = "none";
-  }
-}
-
-
 fetch('/api/overzicht_onderzoeken_organisatie', {
   method: 'GET',
   headers: {
@@ -73,7 +31,7 @@ function showOnderzoeken (onderzoeken) {
                 </td>
             </tr>
           `
-  document.getElementById("onderzoeken_tabel").innerHTML += row;
+  document.querySelector(".js-onderzoektabel").innerHTML += row;
   });
   document.querySelectorAll('.update').forEach(button => {
     button.addEventListener('click', () => {
