3ae829586ac0f8507f88d151a7da044ac05ec1fe
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-14 22:56:39 +0100

    fetch api gebruikt

--- static/javascript/organisatie_aanmaken.js
+++ static/javascript/organisatie_aanmaken.js
@@ -2,13 +2,34 @@ document.getElementById("aanmaken").addEventListener("click",organisatie_aanmake
 
 function organisatie_aanmaken()
 {
+    let api_key = "xd"
     let naam = document.getElementById("naam").value
     let option = document.getElementById("option").value
     let website = document.getElementById("website").value
     let beschrijving = document.getElementById("beschrijving").value
     let contactpersoon = document.getElementById("contactpersoon").value
     let email = document.getElementById("email").value
-    let number = document.getElementById("number").value
+    let number_ = document.getElementById("number").value
+    let number = Number(number)
     let overige_details = document.getElementById("overigedetails").value
 
+    fetch("/api/organisatie_aanmaken/new",{
+        method: "POST",
+        headers:{
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify({
+            "naam": naam,
+            "option":option,
+            "website":website,
+            "beschrijving":beschrijving,
+            "contactpersoon":contactpersoon,
+            "email":email,
+            "number":number,
+            "overige_details":overige_details,
+            "api_key":api_key
+        })
+    })
+    .then((response => response.json()))
+    .then((data => console.log(data)))
 }
