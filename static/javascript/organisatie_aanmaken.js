e55aafcfe2a69e5358b1dfa9c6bd4df8be808f1a
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2025-03-14 21:50:01 +0100

    functie aangemaakt om values te krijgen van ingevoerde velden

---
+++ static/javascript/organisatie_aanmaken.js
@@ -0,0 +1,14 @@
+document.getElementById("aanmaken").addEventListener("click",organisatie_aanmaken)
+
+function organisatie_aanmaken()
+{
+    let naam = document.getElementById("naam").value
+    let option = document.getElementById("option").value
+    let website = document.getElementById("website").value
+    let beschrijving = document.getElementById("beschrijving").value
+    let contactpersoon = document.getElementById("contactpersoon").value
+    let email = document.getElementById("email").value
+    let number = document.getElementById("number").value
+    let overige_details = document.getElementById("overigedetails").value
+
+}
