8ccb1737b962e5e51debc189e33905b7b5641da5
Roos-1104659 <1104659@hr.nl>
2025-03-18 14:17:54 +0100

    white cross image shows up

--- static/javascript/dark_mode.js
+++ static/javascript/dark_mode.js
@@ -1,6 +1,6 @@
 function darkLightMode() {
     document.body.classList.toggle("dark-mode");
-    document.querySelectorAll("tbody, .popup-content, .column, section, .table-clickable th, .modal-content, table, th, td, h1, .administrator-add, .administrator-delete, .administrator-edit").forEach(element => {
+    document.querySelectorAll("tbody, .popup-content, .column, section, .table-clickable th, .modal-content, table, th, td, h1, .administrator-add, .administrator-delete, .administrator-edit, .cross-image").forEach(element => {
         element.classList.toggle("dark-mode");
     });
     if (document.body.classList.contains("dark-mode")) {
@@ -10,6 +10,6 @@ function darkLightMode() {
 
 window.onload = function() {
     if (localStorage.getItem("darkMode") === "enabled") { document.body.classList.add("dark-mode");
-        document.querySelectorAll("tbody, .popup-content, .column, section, .table-clickable th, .modal-content, table, th, td, h1, .administrator-add, .administrator-delete, .administrator-edit").forEach(element => {element.classList.add("dark-mode");
+        document.querySelectorAll("tbody, .popup-content, .column, section, .table-clickable th, .modal-content, table, th, td, h1, .administrator-add, .administrator-delete, .administrator-edit, .cross-image").forEach(element => {element.classList.add("dark-mode");
         })}
     };    
