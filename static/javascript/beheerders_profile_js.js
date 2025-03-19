11c225b61af74bf4fd3dc5f36a9a7ab775be2d80
Djanou-1099994 <djanouoverweg@gmail.com>
2025-03-19 17:33:32 +0100

    feature: made js for beheerders_profile so user info gets imported through an api

---
+++ static/javascript/beheerders_profile_js.js
@@ -0,0 +1,36 @@
+function get_user_id(){
+    fetch('/get_user_id')
+        .then(response => response.json())
+        .then(get_user_info);
+}
+
+function get_user_info(id){
+    let user_id = id.beheerder_id
+    fetch(`/api/administrator/${user_id}`)
+        .then(response => response.json())
+        .then(fill_html)
+}
+
+function fill_html(beheerder_info){
+    let name = document.getElementById('naam');
+    name.innerHTML = '';
+
+    name.innerHTML = `
+    <p>${beheerder_info.volle_naam}</p>
+    <button>change</button>
+    <br>
+    <br>
+    <p>${beheerder_info.wachtwoord}</p>
+    <button>change</button>
+    <br>
+    <br>
+    <p>${beheerder_info.email}</p>
+    <button>change</button>
+    <br>
+    <br>
+    <p>${beheerder_info.telefoonnummer}</p>
+    <button >change</button>
+    `
+}
+
+get_user_id()
